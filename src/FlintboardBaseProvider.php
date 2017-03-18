206a6faf78f9d87a46070a1bc236c4be08d5af68
Niels Steenman <ncsteenman@gmail.com>
2017-03-19 00:39:06 +0100

    First commit

---
+++ src/FlintboardBaseProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace FlintWebmedia\FlintboardBase;
+
+use Illuminate\Support\ServiceProvider;
+
+class FlintboardBaseProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        $this->loadRoutesFrom(__DIR__.'/FlintboardBaseRouter.php');
+
+        $this->publishes([
+            __DIR__.'/config/flintboardbase.php' => config_path('Flintboard/base.php'),
+        ]);
+
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'flintboardbase');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
