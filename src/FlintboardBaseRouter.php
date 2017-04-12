525340a1a65df4031f6aac953a74be8187c3c5e4
Niels Steenman <ncsteenman@gmail.com>
2017-04-12 20:26:48 +0200

    Change dev dependencies

--- src/FlintboardBaseRouter.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function()
-{
-    CRUD::resource('page', 'FlintWebmedia\FlintboardBase\app\Http\Controllers\PageCrudController');
-    CRUD::resource('content', 'FlintWebmedia\FlintboardBase\Controllers\PageEntityCrudController');
-});
